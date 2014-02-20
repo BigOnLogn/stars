1dde19e81162e7f2eb5603007c19828bca7a90e5
Nick Moore <nick65@gmail.com>
2014-02-19 18:36:22 -0600

    Hellophaser

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,12 @@
 require(['phaser'], function(Phaser) {
-  console.log('entry point!');
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create});
+
+  function preload() {
+    game.load.image('logo', 'assets/phaser.png');
+  }
+
+  function create() {
+    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+    logo.anchor.setTo(0.5, 0.5);
+  }
 });
